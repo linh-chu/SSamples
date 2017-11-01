4beac4a40bcf6802f7be3b8cb503bbbd86d20e67
Linh Chu <cnhlinh@gmail.com>
2017-11-01 13:33:02 +1100

    add RxSwift with carthage

---
+++ SSamples/SSamples/Shared/AppStoryboard.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+enum AppStoryboard: String {
+    
+    case main = "Main"
+    case tableView = "TableView"
+    
+    var instance: UIStoryboard {
+        return UIStoryboard(name: self.rawValue, bundle: Bundle.main)
+    }
+    
+    func viewController<T: UIViewController>(_ viewControllerClass: T.Type, function : String = #function, line : Int = #line, file : String = #file) -> T {
+        
+        let storyboardID = "\(viewControllerClass as UIViewController.Type)"
+        guard let vc =  instance.instantiateViewController(withIdentifier: storyboardID) as? T else {
+            fatalError("ViewController with identifier \(storyboardID), not found in \(self.rawValue) Storyboard.\nFile : \(file) \nLine Number : \(line) \nFunction : \(function)")
+        }
+        return vc
+    }
+}
+
+extension UIViewController {
+    
+    static func instantiate(from appStoryboard: AppStoryboard) -> Self {
+        
+        return appStoryboard.viewController(self)
+    }
+}
---
+++ SSamples/SSamples/Shared/RxManager.swift
@@ -0,0 +1,14 @@
+//
+//  RxManager.swift
+//  SSamples
+//
+//  Created by Linh Chu on 1/11/17.
+//  Copyright © 2017 Linh Chu. All rights reserved.
+//
+
+import Foundation
+//import RxSwift
+
+class RxManager {
+    
+}
